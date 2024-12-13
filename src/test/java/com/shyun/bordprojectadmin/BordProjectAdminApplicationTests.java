3347e6d192d5ca7920dbf47515d86b002c0c7560
shyun <tjdgh0312@naver.com>
2024-12-13 16:53:48 +0900

    first commit

---
+++ src/test/java/com/shyun/bordprojectadmin/BordProjectAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shyun.bordprojectadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BordProjectAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
