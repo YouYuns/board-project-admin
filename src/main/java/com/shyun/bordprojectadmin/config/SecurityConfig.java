318710911a752e810bc6be92b9bec5993fc3f199
shyun <tjdgh0312@naver.com>
2024-12-17 23:39:56 +0900

    시큐리티 설정 & oauth2설정

--- src/main/java/com/shyun/bordprojectadmin/config/SecurityConfig.java
+++ src/main/java/com/shyun/bordprojectadmin/config/SecurityConfig.java
@@ -1,10 +1,25 @@
 package com.shyun.bordprojectadmin.config;
 
+import com.shyun.bordprojectadmin.domain.constant.RoleType;
+import com.shyun.bordprojectadmin.dto.security.BoardAdminPrincipal;
+import com.shyun.bordprojectadmin.dto.security.KakaoOAuth2Response;
+import com.shyun.bordprojectadmin.service.AdminAccountService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.Set;
 import java.util.UUID;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -14,15 +29,82 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        String[] rolesAboveManager = {
+                RoleType.MANAGER.name(),
+                RoleType.DEVELOPER.name(),
+                RoleType.ADMIN.name()
+        };
         return http
                 .authorizeHttpRequests(auth -> auth
-                      .anyRequest().permitAll()
+                      .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                      .requestMatchers(HttpMethod.POST, "/**").hasAnyRole(rolesAboveManager)
+                        .requestMatchers(HttpMethod.DELETE, "/**").hasAnyRole(rolesAboveManager)
+                      .anyRequest().authenticated()
                 )
                 .formLogin(withDefaults())
                 .logout(logout -> logout.logoutSuccessUrl("/"))
+                .oauth2Login(withDefaults())
                 .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
                 .build();
     }
 
+    @Bean
+    public UserDetailsService userDetailsService(AdminAccountService adminAccountService) {
+        //바로  username이 가능한 이유는 UserDetailsService는 UserDetail의 loadUserByusername이라는 메서드를 구현해야되기 때문이다.
+        // 해당 메서드는interface이므로 username을 받고있다.
+        return username -> adminAccountService
+                .searchUser(username)
+                .map(BoardAdminPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("유저를 찾을 수 없습니다 - username: " + username));
 
+    }
+
+    /**
+     * <p>
+     * OAuth 2.0 기술을 이용한 인증 정보를 처리한다.
+     * 카카오 인증 방식을 선택.
+     *
+     * @param adminAccountService  게시판 서비스의 사용자 계정을 다루는 서비스 로직
+     * @param passwordEncoder 패스워드 암호화 도구
+     * @return {@link OAuth2UserService} OAuth2 인증 사용자 정보를 읽어들이고 처리하는 서비스 인스턴스 반환
+     */
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(
+            AdminAccountService adminAccountService,
+            PasswordEncoder passwordEncoder
+    ) {
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+        return userRequest -> {
+            OAuth2User oAuth2User = delegate.loadUser(userRequest);
+
+            KakaoOAuth2Response kakaoResponse = KakaoOAuth2Response.from(oAuth2User.getAttributes());
+            String registrationId = userRequest.getClientRegistration().getRegistrationId();
+            String providerId = String.valueOf(kakaoResponse.id());
+            String username = registrationId + "_" + providerId;
+            String dummyPassword = passwordEncoder.encode("{bcrypt}" + UUID.randomUUID());
+            Set<RoleType> roleTypes = Set.of(RoleType.USER);
+
+            return adminAccountService.searchUser(username)
+                    .map(BoardAdminPrincipal::from)
+                    .orElseGet(() ->
+                            BoardAdminPrincipal.from(
+                                    adminAccountService.saveUser(
+                                            username,
+                                            dummyPassword,
+                                            roleTypes,
+                                            kakaoResponse.email(),
+                                            kakaoResponse.nickname(),
+                                            null
+                                    )
+                            )
+                    );
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
